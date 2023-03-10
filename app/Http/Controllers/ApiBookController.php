9d9c722ab1d10d08ffa83208e6bf7822366e8a78
Xafy <57871519+Xafy@users.noreply.github.com>
2023-03-10 22:02:04 +0200

    books crud api

--- app/Http/Controllers/ApiBookController.php
+++ app/Http/Controllers/ApiBookController.php
@@ -2,45 +2,38 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Book;
-use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
-class BookController extends Controller
+class ApiBookController extends Controller
 {
+    //
     public function index(){
-        $books = Book::orderBy('id', 'DESC')->paginate(6);
-        // $books = Book::select('title', 'desc')->get();
-        // $books = Book::where('id', '=', 1)->get();
-        // $books =  Book::select('title', 'desc')->where('id', '=', 1)->get();
-        // $books = Book::select('title', 'desc')->where('id', '=', 1)->orderBy('id', 'DESC')->get();   
-        return view('books.index', compact('books'));
-    }
+        $books = Book::select('id', 'title')->get();
 
+        return response()->json($books);
+    }
+    
     public function show($id){
-        // $book = Book::where('id', '=', $id)->first();
         $book = Book::findOrFail($id);
-        return view('books.show', compact('book'));
+
+        return response()->json($book);
     }
 
-    public function createForm(){
-        $categories = Category::select('id', 'name')->get();
-        return view('books.create', compact('categories'));
-    }
-
-    public function somthing(){
-        return view('books.somthing');
-    }
-
-    public function storeBook(Request $req){
-
-        $req->validate([
+    public function store(Request $req){
+        $validator = Validator::make($req->all(), [
             'title' => 'required|string|max:50',
             'desc' => 'required|string',
             'img' => 'image|mimes:jpg,png,jpeg',
             'categories' => 'required',
             'categories.*' => 'exists:categories,id'
         ]);
+
+        if($validator->fails()){
+            $errors = $validator->errors();
+            return response()->json($errors);
+        }
 
         $img = $req->file('img');
         $ext = $img->getClientOriginalExtension();
@@ -55,21 +48,23 @@ class BookController extends Controller
 
         $book->categories()->sync($req->categories);
 
-        return redirect(route('books.index')); 
+        return response()->json(["message" => "book created successfuly", "book" => $book]);
     }
-
-    public function editBook($id){
-        $book = Book::findOrFail($id);
-        return view('books.edit', compact('book'));
-    }
-
-    public function updateBook($id, Request $req){
-
-        $req->validate([
+    
+    public function update($id, Request $req){
+        
+        $validator = Validator::make($req->all(), [
             'title' => 'required|string|max:50',
             'desc' => 'required|string',
-            'img' => 'nullable|image|mimes:jpg,png,jpeg'
+            'img' => 'image|mimes:jpg,png,jpeg',
+            'categories' => 'required',
+            'categories.*' => 'exists:categories,id'
         ]);
+
+        if($validator->fails()){
+            $errors = $validator->errors();
+            return response()->json($errors);
+        }
 
         $book = Book::findOrFail($id);
 
@@ -84,23 +79,23 @@ class BookController extends Controller
             $img->move(public_path('uploads/books'), $name);
         }
 
+        $book->update([
+            'title' => $req->title,
+            'desc' => $req->desc,
+            'img' => $name
+        ]);
+        $book->categories()->sync($req->categories);
 
-        $book->title = $req->title;
-        $book->desc = $req->desc;
-        $book->img = $name;
-
-        $book->save();
-
-        return redirect(route('books.show', $id));
+        return response()->json("book updated");
     }
-
-    public function deleteBook($id){
+    
+    public function delete($id){
         $book = Book::findOrFail($id);
         if($book->img !== null){
             unlink(public_path('uploads/books/') . $book->img);
         }
         $book->categories()->sync([]);
         Book::destroy($id);
-        return redirect(route('books.index'));
+        return response()->json("book deleted");
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiBookController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/books', [ApiBookController::class, 'index']);
+Route::get('/books/{id}', [ApiBookController::class, 'show']);
+Route::post('/books', [ApiBookController::class, 'store']);
+Route::patch('/books/{id}', [ApiBookController::class, 'update']);
+Route::delete('/books/{id}', [ApiBookController::class, 'delete']);
